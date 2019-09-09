967b1b6d394b7537427737715a71f4318421afe1
Adela Rahman <arahman@users.campus>
2019-09-09 17:21:22 -0500

    brontowurt and nuggets

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public uint nuggetCount = 6;
+        public double nuggetPrice = 0.25;
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { nuggetCount + "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 59*6;
+        }
+
+        public void AddNugget()
+        {
+            nuggetCount++;
+            Price += nuggetPrice;
+            Calories += 59;
+        }
+
+
+    }
+}
